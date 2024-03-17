e95daec5c96485d09d51c32f3cbe4893222e1cb7
Perekcs <andriy.perekupka@gmail.com>
2024-03-18 01:32:48 +0200

    Added functions headers with docstrings in input.py and output.py

---
+++ app/io/input.py
@@ -0,0 +1,17 @@
+def consoleInput():
+    """
+    Function to input text from the console.
+    """
+    pass
+
+def fileInput():
+    """
+    Function to read from a file.
+    """
+    pass
+
+def pandasInput():
+    """
+    Function to read from a file using the pandas.
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+def consoleOutput():
+    """
+    Function to output text to the console.
+    """
+    pass
+
+def fileOutput():
+    """
+    Function to write to a file.
+    """
+    pass
